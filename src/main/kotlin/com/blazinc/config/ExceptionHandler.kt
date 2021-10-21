2dd4c365530576cd6998c294f05897b6bd13019f
Gerson <tnii.gerson@gmail.com>
2021-10-21 19:55:55 +0200

    Exception handler

---
+++ src/main/kotlin/com/blazinc/config/ExceptionHandler.kt
@@ -0,0 +1,19 @@
+package com.blazinc.config
+
+import io.micronaut.context.annotation.Requires
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Produces
+import io.micronaut.http.server.exceptions.ExceptionHandler
+import jakarta.inject.Singleton
+
+@Produces
+@Singleton
+@Requires(classes = [Exception::class, ExceptionHandler::class])
+class ExceptionHandler :
+    ExceptionHandler<Exception?, HttpResponse<*>> {
+    override fun handle(request: HttpRequest<*>?, exception: Exception?): HttpResponse<*> {
+        println(exception?.message)
+        return HttpResponse.ok(0)
+    }
+}

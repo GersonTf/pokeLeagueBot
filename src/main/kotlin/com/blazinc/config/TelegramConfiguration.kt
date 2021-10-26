11868ce5ee5b804fe5016c8018b22be238c35d2b
Gerson <tnii.gerson@gmail.com>
2021-10-26 19:12:56 +0200

    hiding telegram token

---
+++ src/main/kotlin/com/blazinc/config/TelegramConfiguration.kt
@@ -0,0 +1,8 @@
+package com.blazinc.config
+
+class TelegramConfiguration {
+
+    companion object {
+        const val TELEGRAM_API_URL = "https://api.telegram.org"
+    }
+}
--- src/main/kotlin/com/blazinc/service/MessageService.kt
+++ src/main/kotlin/com/blazinc/service/MessageService.kt
@@ -1,18 +1,23 @@
 package com.blazinc.service
 
+import com.blazinc.config.TelegramConfiguration
+import io.micronaut.context.annotation.Value
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import jakarta.inject.Singleton
 
 @Singleton
-class MessageService(@param:Client("https://api.telegram.org/bot496020190:AAHv_q7IUeqDMCVW0UuIIq3t4uh1tok8FK8") private val httpClient: HttpClient) {
+class MessageService(@param:Client(TelegramConfiguration.TELEGRAM_API_URL) private val httpClient: HttpClient) {
+
+    @Value("\${telegram.token}")
+    lateinit var botToken: String
 
     /**
      * It sends a message to a specific telegram chatId
      * @param message to send and chatId
      */
     fun sendNotificationToTelegram(message: String, chatId: String) {
-        val uri = "/sendMessage?text=$message&chat_id=$chatId"
+        val uri = "/$botToken/sendMessage?text=$message&chat_id=$chatId"
         httpClient.toBlocking().retrieve(uri)
     }
 }
